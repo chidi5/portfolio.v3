c40bdd396993fe994caa2d2a12f947b956593c5f
chidi5 <joshdash.ji@gmail.com>
2024-02-04 22:39:47 +0100

    adding another project

--- app/ProjectSection.tsx
+++ app/ProjectSection.tsx
@@ -2,6 +2,7 @@ import { Flex, Heading, Section } from "@radix-ui/themes";
 import React from "react";
 import Link from "next/link";
 import issueTracker from "./images/issue-tracker.png";
+import fastKeys from "./images/fast-keys.png";
 import Image from "next/image";
 
 const ProjectSection = () => {
@@ -17,6 +18,122 @@ const ProjectSection = () => {
       </div>
       <div>
         <ul>
+          <li className="mb-12 group">
+            <Link
+              href="https://issue-tracker-ten-plum.vercel.app"
+              target="_blank"
+            >
+              <Flex
+                direction={{ initial: "column-reverse", sm: "row" }}
+                gap="4"
+                className="p-4 rounded-md backdrop-blur-sm group-hover:bg-slate-400 group-hover:bg-opacity-10"
+              >
+                <div>
+                  <Image
+                    src={issueTracker}
+                    alt="issue tracker web app"
+                    width={200}
+                    height={200}
+                    className="border-2 border-gray-300 rounded-md"
+                  />
+                </div>
+                <div className="w-full">
+                  <Heading
+                    as="h3"
+                    size="3"
+                    className="group-hover:text-indigo-500"
+                  >
+                    Issue Tracking App
+                  </Heading>
+                  <p className="mt-2 text-sm leading-normal font-medium">
+                    Web app for efficient issue tracking. Create, save, and
+                    assign issues seamlessly. Track the status of each issue,
+                    whether it&apos;s open, closed, or in progress. Stay
+                    organized and streamline your workflow with this
+                    user-friendly issue-tracking tool for effective project
+                    management.
+                  </p>
+                  <ul
+                    className="mt-2 flex flex-wrap"
+                    aria-label="Technologies used:"
+                  >
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-indigo-400/10 px-3 py-1 text-xs font-medium leading-5 text-indigo-400 ">
+                        Next.js
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-indigo-400/10 px-3 py-1 text-xs font-medium leading-5 text-indigo-400 ">
+                        TypeScript
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-indigo-400/10 px-3 py-1 text-xs font-medium leading-5 text-indigo-400 ">
+                        Prisma
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-indigo-400/10 px-3 py-1 text-xs font-medium leading-5 text-indigo-400 ">
+                        PlanetScale
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </Flex>
+            </Link>
+          </li>
+          <li className="mb-12 group">
+            <Link href="https://fast-keys.vercel.app" target="_blank">
+              <Flex
+                direction={{ initial: "column-reverse", sm: "row" }}
+                gap="4"
+                className="p-4 rounded-md backdrop-blur-sm group-hover:bg-slate-400 group-hover:bg-opacity-10"
+              >
+                <div>
+                  <Image
+                    src={fastKeys}
+                    alt="A typing game"
+                    width={200}
+                    height={50}
+                    className="border-2 border-gray-300 rounded-md"
+                  />
+                </div>
+                <div className="w-full">
+                  <Heading
+                    as="h3"
+                    size="3"
+                    className="group-hover:text-indigo-500"
+                  >
+                    Fast keys
+                  </Heading>
+                  <p className="mt-2 text-sm leading-normal font-medium">
+                    A user interactive typing game that tracks the typist
+                    mistakes, words per minute and characters per minute.
+                  </p>
+                  <ul
+                    className="mt-2 flex flex-wrap"
+                    aria-label="Technologies used:"
+                  >
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-indigo-400/10 px-3 py-1 text-xs font-medium leading-5 text-indigo-400 ">
+                        Next.js
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-indigo-400/10 px-3 py-1 text-xs font-medium leading-5 text-indigo-400 ">
+                        TypeScript
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-indigo-400/10 px-3 py-1 text-xs font-medium leading-5 text-indigo-400 ">
+                        Radix UI
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </Flex>
+            </Link>
+          </li>
           <li className="mb-12 group">
             <Link
               href="https://issue-tracker-ten-plum.vercel.app"
